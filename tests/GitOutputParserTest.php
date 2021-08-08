3b312ac60e6f93e2cd5dd157f527e6df50442d81
Julien Desrosiers <hello@juliendesrosiers.com>
2021-08-07 23:52:07 -0400

    Add another test

--- tests/GitOutputParserTest.php
+++ tests/GitOutputParserTest.php
@@ -56,4 +56,24 @@ TXT;
             count(GitOutputParser::get_checkout_candidates($test_output))
         );
     }
+
+    public function testCheckoutCandidatesInChangesOnlyButDifferent(): void
+    {
+        $test_output = <<<TXT
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   readme.html
+        modified:   wp-config-sample.php
+
+no changes added to commit (use "git add" and/or "git commit -a")
+TXT;
+         $this->assertEquals(
+            2,
+            count(GitOutputParser::get_checkout_candidates($test_output))
+        );
+    }
 }
